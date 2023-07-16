9574d991ebba2f6079fc6b40ae27a58bb5491b94
Oscar Espitia <odespitia@sena.edu.co>
2023-07-16 02:39:39 -0500

    tema 5

--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -5,8 +5,155 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 5
-      h1 Modelos administrativos
+      h1 Norma ISO 9001
 
+    p.mb-5(data-aos="fade-down") La calidad es una característica propia y natural de todo producto, bien o servicio, la cual es evidenciada por los clientes y su grado de satisfacción al momento de su consumo o uso. Es en este momento que este concepto tiene validez real, razón por la cual las empresas se preocupan por identificarlo de forma permanente, verificando la correcta agregación de los valores y el cumplimiento de los preceptos misionales. Anteriormente, las empresas revisaban la idoneidad de cada uno de los productos, lo que conllevaba elevados costos. Con el tiempo, se concluyó que una vez realizado el análisis sistémico, en los que los productos dependen de procesos controlados que tienen una oportuna y correcta agregación de los valores, es posible garantizar la calidad esperada. La empresa define la forma de elaborar o desarrollar sus actividades para alcanzar este fin, estableciendo un sentido de disciplina en el cumplimiento de estos procesos. 
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-12.col-md-8.col-lg-9
+        .bloque-texto-d.color-primario.p-4(data-aos="fade-down")
+          .bloque-texto-d__texto.mb-2
+            i.fas.fa-quote-left
+            p.mb-0 En esta evolución de refinamiento de la calidad, surge la Organización Internacional de Estandarización (ISO, por sus siglas en inglés); organización privada, autónoma e independiente que se dedica a establecer lineamientos que favorecen la uniformidad de los procesos en las empresas, que los aplican en los distintos sectores productivos de bienes, productos o servicios.
+          .bloque-texto-d__autor 
+            .h6.mb-0 - ICONTEC, 2015
+              i.fas.fa-quote-right
+      .col-12.col-md-4.col-lg-3(data-aos="fade-down")
+        figure
+          img(src='@/assets/curso/temas/tema5/tema5-01.svg', alt='') 
+
+    .bloque-texto-a.color-primario.p-3.p-md-3.mb-5(data-aos="fade-left")
+      .row.m-0.align-items-center.justify-content-between
+        .col-lg-4.mb-4.mb-lg-0
+          h3.mb-0 ¿Qué son la familia de lineamientos o normas ISO 9000?
+        .col-lg-8
+          .bloque-texto-a__texto.p-3
+            p Son un conjunto de indicaciones que las empresas deben llevar a normas de ordenamiento y cumplimiento, que establecen las directrices para conseguir una determinada calidad del producto o servicio final, que también ayudan a definir un sistema de gestión de procesos de la misma.
+
+    p.mb-3(data-aos="fade-down") En sus inicios, estos lineamientos se idearon para empresas que producían productos materiales, excluyendo las compañías que se dedicaban a la prestación de servicios. Posteriormente, mediante diversas modificaciones y actualizaciones que sufrieron hacia el año 2000 se incluyó cualquier tipo de empresa.
+    p.mb-5(data-aos="fade-down") A continuación, se presentan algunas de estas normas:      
+
+    .row.justify-content-center.align-items-center.mb-4
+      .col-md-11.col-lg-10.mb-4(data-aos="fade-left")
+        .tarjeta.tarjeta--gris.p-3
+          .row.justify-content-around.align-items-center
+            .col-3.col-sm-2.col-lg-1
+              img(src="@/assets/curso/temas/tema5/tema5-02.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  p.text-small.mb-0 Establece las directrices y principios básicos de calidad, así como el vocabulario de un sistema de gestión de calidad, Se encarga de definir la terminología utilizada de manera común, de tal manera que no haya diferencias de conceptos al momento de auditorías de calidad o de procesos en las empresas.
+      .col-md-11.col-lg-10.mb-4(data-aos="fade-left")
+        .tarjeta.tarjeta--gris.p-3
+          .row.justify-content-around.align-items-center
+            .col-3.col-sm-2.col-lg-1
+              img(src="@/assets/curso/temas/tema5/tema5-03.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  p.text-small.mb-0 Define el conjunto de requisitos mínimos en un sistema de gestión de la calidad, para que las empresas tengan un ejercicio eficiente. Es la norma más aplicada, ya que determina los mínimos estándares que deben alcanzar las empresas en una adecuada gestión, con el fin de obtener sus propósitos de eficiencia y cumplimiento de su promesa de valor al cliente, garantizando que los bienes, productos o servicios tengan idénticas características. Es de obligatorio cumplimiento.
+      .col-md-11.col-lg-10.mb-4(data-aos="fade-left")
+        .tarjeta.tarjeta--gris.p-3
+          .row.justify-content-around.align-items-center
+            .col-3.col-sm-2.col-lg-1
+              img(src="@/assets/curso/temas/tema5/tema5-04.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  p.text-small.mb-0 Busca la mejora continua en el desempeño del sistema de gestión de calidad en las empresas, logrando niveles de eficiencia por rendimientos, innovación procedimental; procura que se den mejores resultados en los procesos.
+
+    p.mb-5(data-aos="fade-down") Ahora, veamos algunas diferencias entre la familia de normas ISO 9000:
+    
+    .row.justify-content-center.align-items-center.mb-4(data-aos="fade-down")
+      .col-12.col-lg-10.col.md-11.mb-4.mb-md-0
+        .tabla-a.color-primario.mb-4
+          table
+            thead
+              tr
+                th ISO 9000
+                th ISO 9001
+                th ISO 9004
+              tr
+                td Se ocupa del vocabulario y los principios básicos para que las empresas y clientes hablen el mismo idioma.
+                td Define los requisitos y lineamientos de cumplimiento que establecerá la empresa para garantizar la calidad
+                td Promueve el mejoramiento continuo de las prácticas y otros procesos de la empresa.
+
+    .row.align-items-center.justify-content-center.mb-4(data-aos="fade-down-left")
+      .col-12.col-md-10.col-lg-8
+        .cajon.color-acento-contenido.p-3
+          p.mb-0 La finalidad general de la aplicación de las normas ISO 9001 es lograr que las empresas consigan niveles de calidad sostenible y la satisfacción del cliente, mediante el establecimiento de los procesos y una mejora continua periódica.
+    
+    .row.justify-content-around.align-items-center.mb-5
+      .col-12.col-md-6(data-aos="flip-left")
+        img(src="@/assets/curso/temas/tema5/tema5-05.png")
+      .col-12.col-md-6(data-aos="flip-right")
+        p La norma ISO 9000 se divide en siete lineamientos básicamente:
+        ul.lista-ul.mb-2
+          li.mb-0
+            i.fas.fa-angle-right
+            | Enfoque al cliente
+          li.mb-0
+            i.fas.fa-angle-right
+            | Liderazgo gerencial.
+          li.mb-0
+            i.fas.fa-angle-right
+            | Participación del equipo de trabajo o personal de la empresa .
+          li.mb-0
+            i.fas.fa-angle-right
+            | Enfoque basado en los procesos empresariales. 
+          li.mb-0
+            i.fas.fa-angle-right
+            | Mejora continua.
+          li.mb-0
+            i.fas.fa-angle-right
+            | Enfoque basado en hechos que soporten la toma de decisiones.
+          li.mb-0
+            i.fas.fa-angle-right
+            | Gestión de las relaciones.
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-12.col-md-10.mb-4.mb-md-0(data-aos="fade-left")
+        .tarjeta-a.color-primario.p-3
+          .row.justify-content-around.align-items-center
+            .col-3.col-sm-2.col-lg-2
+              img(src="@/assets/curso/temas/tarea-ok.svg")
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  h4 Norma ISO 9001
+                  p.text-small Para ampliar sobre los aspectos propios de cada enfoque y la manera de aplicación según el enfoque de ciclo PHVA, con el cual debe operar la norma, se invita a consultar el anexo: Norma ISO 9001.
+                .col-sm-auto
+                  a.boton.color-acento-botones(href="https://colaboracion.dnp.gov.co/CDT/Normograma/NORMA%20ISO%209001%202015.pdf" target="_blank")
+                    span Link
+                    i.fas.fa-link
+
+    p.mb-5(data-aos="fade-down") Entre los principales fines que se buscan con la aplicación de la norma ISO 9001 se encuentra:
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-md-6.col-xl.mb-4.mb-xl-0(data-aos="fade-right")
+        .tarjeta-numerada.color-primario.ps-5.py-4.pe-3
+          .tarjeta-numerada__numero
+            .h2 1
+          p.text-small Elevar y proyectar una mejor imagen y confianza ante el cliente. Al tener una certificación oficial de un organismo tan importante y reconocido como ISO, los clientes tienen más confianza en la empresa y en los productos o servicios que ofrece.
+
+      .col-md-6.col-xl.mb-4.mb-xl-0(data-aos="fade-down")
+        .tarjeta-numerada.color-secundario.ps-5.py-4.pe-3
+          .tarjeta-numerada__numero
+            .h2 2
+          p.text-small Elevar los niveles de satisfacción de los clientes, ya que en sus procesos de mejora se puede identificar, necesidades distintivas, a las que si se da respuesta, se incrementa la fidelización de los clientes (expresión de elevados niveles de satisfacción de los clientes).
+
+      .col-md-6.col-xl.mb-4.mb-xl-0(data-aos="fade-down")
+        .tarjeta-numerada.color-acento-contenido.ps-5.py-4.pe-3
+          .tarjeta-numerada__numero
+            .h2 3
+          p.text-small Crear una cultura de calidad y refinamiento para la mejora continua en los trabajadores y equipos de trabajo de la empresa. Los empleados son un aliado estratégico de la empresa y son clave en este proceso, pues generan iniciativas.
+
+      .col-md-6.col-xl.mb-4.mb-xl-0(data-aos="fade-left")
+        .tarjeta-numerada.color-acento-botones.ps-5.py-4.pe-3
+          .tarjeta-numerada__numero
+            .h2 4
+          p.text-small Elevar los niveles de eficiencia en las actividades y en los procesos de la empresa, lo que se expresa en la reducción de costos y gastos, coadyuvando a la generación de utilidades.
+  
 
 
 </template>
